e2d3984e5f1f4f8e4816b7f662cae842b0f0434d
Elek Augusto V. M. Guidolin <elekguidolin@gmail.com>
2018-01-03 19:37:19 -0200

    I've defined a dependency for HttpClient, using a constructor parameter, created a method for get, and called the get method passing in the Url, used generics to specify the returned type and also added an error handling to the code. I've also configured the subscribe in the component to make all work together.

--- APM/src/app/products/product-list.component.ts
+++ APM/src/app/products/product-list.component.ts
@@ -14,6 +14,7 @@ export class ProductListComponent implements OnInit {
   imageWidth: number = 50;
   imageMargin: number = 2;
   showImage: boolean = false;
+  errorMessage: string;
 
   _listFilter: string;
   get listFilter(): string {
@@ -47,8 +48,13 @@ export class ProductListComponent implements OnInit {
 
   ngOnInit(): void {
     console.log('In OnInit');
-    this.products = this._productService.getProducts();
-    this.filteredProducts = this.products;
+    this._productService.getProducts()
+      .subscribe(products => {
+        this.products = products;
+        this.filteredProducts = this.products
+      },
+        error => this.errorMessage = <any>error);
+    ;
   }
 
 }
