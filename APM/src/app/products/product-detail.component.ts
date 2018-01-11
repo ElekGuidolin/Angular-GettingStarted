bce6a620f854831398f63100f24f56e6d6b24f76
Elek Augusto V. M. Guidolin <elekguidolin@gmail.com>
2018-01-11 10:32:16 -0200

    I've configurated the angular router to send and receive parameters, I've also created a guard service using the Angular CLI to protect the route.

--- APM/src/app/products/product-detail.component.ts
+++ APM/src/app/products/product-detail.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 import { IProduct } from './product';
 
@@ -11,9 +12,28 @@ export class ProductDetailComponent implements OnInit {
   pageTitle: string = 'Product Detail';
   product: IProduct;
 
-  constructor() { }
+  constructor(private _route: ActivatedRoute,
+              private _router: Router) { }
 
   ngOnInit() {
+    //+ is a javascript shortcut to convert a string value into a number value.
+    let id = +this._route.snapshot.paramMap.get('id');
+    this.pageTitle += `: ${id}`;
+    this.product = {
+      "productId": id,
+      "productName": "Saw",
+      "productCode": "TBX-0022",
+      "releaseDate": "May 15, 2016",
+      "description": "15-inch steel blade hand saw",
+      "price": 11.55,
+      "starRating": 3.7,
+      "imageUrl": "http://openclipart.org/image/300px/svg_to_png/27070/egore911_saw.png"
+    }
+
+  }
+
+  onBack(): void {
+    this._router.navigate(['/products']);
   }
 
 }
